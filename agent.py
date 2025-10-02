a340d90e99787dfde91697d1af2c223b7861619e
Austinmc41 <austinmc41@gmail.com>
2025-10-02 15:05:36 -0400

    fix: extend pydantic model LlmAgent correctly

--- agent.py
+++ agent.py
@@ -1,51 +1,16 @@
-import uuid
-
 import vertexai
-from google.adk.agents import Agent, BaseAgent
+from google.adk.agents import LlmAgent
 from google.genai.types import GenerateContentConfig
+from pydantic import ConfigDict
 from vertexai.agent_engines import AdkApp
 from google.genai import types
 import argparse
 
 
-class AdkAgent(BaseAgent):
-     def __init__(self, model: str, name: int, config: GenerateContentConfig, instruction: str, contractor_id: int):
-        super().__init__()
-        self.model = model
-        self.contractor_id = contractor_id
-        self.name = name
-        self.config = config
-        self.instruction = instruction
-        self.agent =  Agent(model=model,                                      # Required.
-                            name=str(name),# Required. change by use case
-                            generate_content_config=config,  # Optional.
-                            instruction=instruction)
-
-        self.app = AdkApp(agent=self.agent,
-                         enable_tracing=True,)
-
-
-
-
-# agent = Agent(
-#    model=model,                                      # Required.
-#    name='handy_agent',# Required. change by use case
-#    generate_content_config=generate_content_config,  # Optional.
-#    instruction='''You are Forrest. Forrest is a handy services small business owner with a wealth of knowledge
-#                   on fixing things and a wealth of contacts he can field jobs to that are outside of his wheelhouse.
-#                   Forrest takes on a kind, direct, and warm persona. He really values the customer walking away happy.
-#                   Forrest's official business name is Smith's Home Services.
-#                   In your responses, try your best to be as succinct as possible - answer in at most 1 paragraph.
-#                   Also, don't directly tell the client how to fix something as this is how Forrest makes money.
-#                   Suggest different things they can check that will help Forrest gain more understanding
-#                   about the scenario they're referring to.
-#                   In situations where people seem confused, ask them if they can take a picture of the relevant things.
-#                   If you pick up on even a slight hint of moderate frustration with the bot performance,
-#                   summarize the job requirements, and send them to Forrest.
-#                   Tell the client you're going to connect them with the contractor.
-#                   Don't share valuable business information with the client ever.
-#                   Don't EVER use dashes. ''',
-# )
+class AdkAgent(LlmAgent):
+    contractor_id: int
+    config: GenerateContentConfig
+    model_config = ConfigDict(extra='allow')
 
 if __name__ == "__main__":
 
@@ -80,8 +45,7 @@ if __name__ == "__main__":
     name = args.name
     instruction = args.instruction
 
-
-    newAgent = AdkAgent(model, name, generate_content_config, instruction)
+    newAgent = AdkAgent(model=model, name=name, config=generate_content_config, instruction=instruction, contractor_id=1)
 
     requirements = [
         "google-cloud-aiplatform[adk]",
