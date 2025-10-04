3657149cb3306e8fa7e452419853b5faeb20e46f
Austinmc41 <austinmc41@gmail.com>
2025-10-03 20:19:52 -0400

    add agent and contractor

--- sql/handy-bot.sql
+++ sql/handy-bot.sql
@@ -1,5 +1,7 @@
 DROP TABLE adk_session;
 DROP TABLE bot_user;
+DROP TABLE contractor;
+DROP TABLE agent
 
 CREATE TABLE bot_user
 (
@@ -18,5 +20,29 @@ CREATE TABLE adk_session
     FOREIGN KEY (session_id) REFERENCES bot_user(user_id)
 );
 
+CREATE TABLE contractor
+(
+contractor_id INT PRIMARY KEY,
+name TEXT,
+created_at TIMESTAMP,
+email TEXT,
+phone_no VARCHAR(15),
+);
+
+CREATE TABLE agent
+(
+id INT PRIMARY KEY,
+created_at TIMESTAMP,
+contractor_id INT
+
+FOREIGN KEY contractor_id REFERENCES contractor(contractor_id)
+);
+
 CREATE INDEX idx_phone_no
 ON bot_user(phone_no);
+
+CREATE INDEX idx_phone_no
+ON contractor(phone_no);
+
+CREATE INDEX idx_contractor_id_agent
+ON agent(contractor_id);
