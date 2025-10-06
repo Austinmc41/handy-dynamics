9db258b0dc854898b323f51f0c0592303a0920de
Austinmc41 <austinmc41@gmail.com>
2025-10-06 14:08:10 -0400

    add foreign key mapping agent to contractor

--- api/models/db_models/agent_db.py
+++ api/models/db_models/agent_db.py
@@ -1,5 +1,8 @@
-from sqlalchemy import Column, Integer, String, DateTime
+from sqlalchemy import Column, Integer, DateTime, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import Mapped, relationship, mapped_column
+
+from api.models.db_models.contractor_db import ContractorDB
 
 Base = declarative_base()
 
@@ -7,4 +10,6 @@ class AgentDB(Base):
     __tablename__ = "agent"
     id = Column(Integer, primary_key=True, index=True)
     created_at = Column(DateTime)
-    contractor_id = Column(Integer)
+    contractor_id : Mapped[int] = mapped_column(ForeignKey("contractor.contractor_id"))
+
+    contractor: Mapped["ContractorDB"] = relationship(back_populates="agent")
